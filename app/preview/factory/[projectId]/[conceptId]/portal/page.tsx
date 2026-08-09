0c0ef9fb8a9b5cb27697a4fc3d223ac504ef1343
The Architect <brickmail1@gmail.com>
2026-08-09 07:30:35 -0400

    fix: restore Link import in factory portal preview

--- app/preview/factory/[projectId]/[conceptId]/portal/page.tsx
+++ app/preview/factory/[projectId]/[conceptId]/portal/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { CSSProperties } from 'react';
 import { resolveConceptPreviewDraft } from '@/lib/factory-concept-previews';
 import '@/app/components/experience/themes/amanda-editorial/amanda-editorial.css';
